65598368ea2db3e68abd6f3adcb144dc0718aac1
Lounah <30981413+Lounah@users.noreply.github.com>
2018-04-06 21:56:31 +0300

    di setup

--- app/src/main/java/com/lounah/gallery/GalleryApplication.java
+++ app/src/main/java/com/lounah/gallery/GalleryApplication.java
@@ -1,8 +1,39 @@
 package com.lounah.gallery;
 
-/**
- * Created by lounah on 06/04/2018.
- */
 
-public class GalleryApplication {
+import com.lounah.gallery.di.DaggerAppComponent;
+import com.squareup.leakcanary.LeakCanary;
+
+import dagger.android.AndroidInjector;
+import dagger.android.support.DaggerApplication;
+import timber.log.Timber;
+
+public class GalleryApplication extends DaggerApplication {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        installTimber();
+        installLeakCanary();
+
+    }
+
+    private void installTimber() {
+        if (BuildConfig.DEBUG) {
+            Timber.plant(new Timber.DebugTree());
+        }
+    }
+
+    private void installLeakCanary() {
+        if (LeakCanary.isInAnalyzerProcess(this)) {
+            return;
+        }
+        LeakCanary.install(this);
+    }
+
+    @Override
+    protected AndroidInjector<? extends DaggerApplication> applicationInjector() {
+        return DaggerAppComponent.builder().application(this).build();
+    }
 }
