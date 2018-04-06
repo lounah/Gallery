c92a6123ae644533c148b62846a830f01f9e0bca
Lounah <30981413+Lounah@users.noreply.github.com>
2018-04-06 21:54:42 +0300

    ui

---
+++ app/src/main/java/com/lounah/gallery/GalleryApplication.java
@@ -0,0 +1,8 @@
+package com.lounah.gallery;
+
+/**
+ * Created by lounah on 06/04/2018.
+ */
+
+public class GalleryApplication {
+}
--- app/src/main/java/com/lounah/gallery/ui/MainActivity.java
+++ app/src/main/java/com/lounah/gallery/ui/MainActivity.java
@@ -1,4 +1,4 @@
-package com.lounah.gallery;
+package com.lounah.gallery.ui;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -12,6 +12,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import com.lounah.gallery.R;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
