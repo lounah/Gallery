12e2f6c4876c22bab957233edfb81610b79504f5
Lounah <30981413+Lounah@users.noreply.github.com>
2018-04-09 21:47:21 +0300

    base repository update

--- app/src/main/java/com/lounah/gallery/data/repository/BaseRepository.java
+++ app/src/main/java/com/lounah/gallery/data/repository/BaseRepository.java
@@ -1,14 +1,19 @@
 package com.lounah.gallery.data.repository;
 
 
+import android.arch.lifecycle.LiveData;
+
+import com.lounah.gallery.data.entity.Resource;
+
 import java.util.List;
 
 import io.reactivex.Completable;
+import io.reactivex.Observable;
 import io.reactivex.Single;
 
 public interface BaseRepository<T> {
 
-    Single<List<T>> get();
+    LiveData<Resource<List<T>>> get();
 
     Completable add(final T data);
 
