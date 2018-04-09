4a1cffbdffbc5c8f02cc0262e2437ea850eb5937
Lounah <30981413+Lounah@users.noreply.github.com>
2018-04-09 21:46:00 +0300

    created class-wrapper for remote data

--- app/src/main/java/com/lounah/gallery/data/entity/Resource.java
+++ app/src/main/java/com/lounah/gallery/data/entity/Resource.java
@@ -15,25 +15,21 @@ public class Resource<T> {
     @Nullable
     public final T data;
 
-    @Nullable
-    public final String message;
-
-    private Resource(@NonNull Status status, @Nullable T data, @Nullable String message) {
+    private Resource(@NonNull Status status, @Nullable T data) {
         this.status = status;
         this.data = data;
-        this.message = message;
     }
 
     public static <T> Resource<T> success(@NonNull T data) {
-        return new Resource<>(SUCCESS, data, null);
+        return new Resource<>(SUCCESS, data);
     }
 
-    public static <T> Resource<T> error(String msg, @Nullable T data) {
-        return new Resource<>(ERROR, data, msg);
+    public static <T> Resource<T> error(@Nullable T data) {
+        return new Resource<>(ERROR, data);
     }
 
     public static <T> Resource<T> loading(@Nullable T data) {
-        return new Resource<>(LOADING, data, null);
+        return new Resource<>(LOADING, data);
     }
 
 }
