e3560c440967c2e838da1805229f42d4a98f6c9e
Lounah <30981413+Lounah@users.noreply.github.com>
2018-04-09 21:49:21 +0300

    created loading views; added view callbacks

--- app/src/main/java/com/lounah/gallery/ui/feed/FeedFragment.java
+++ app/src/main/java/com/lounah/gallery/ui/feed/FeedFragment.java
@@ -1,17 +1,27 @@
 package com.lounah.gallery.ui.feed;
 
+import android.arch.lifecycle.ViewModelProvider;
+import android.arch.lifecycle.ViewModelProviders;
 import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.app.DialogFragment;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
+import com.afollestad.materialdialogs.MaterialDialog;
 import com.lounah.gallery.R;
 import com.lounah.gallery.data.entity.Photo;
 import com.lounah.gallery.ui.BaseFragment;
+import com.lounah.gallery.ui.MainActivity;
+
+import java.util.List;
 
 import javax.inject.Inject;
 
@@ -19,101 +29,135 @@ import butterknife.BindView;
 import butterknife.ButterKnife;
 import timber.log.Timber;
 
-public class FeedFragment extends BaseFragment implements PhotoOnClickListener {
+public class FeedFragment extends BaseFragment implements SwipeRefreshLayout.OnRefreshListener {
 
     @BindView(R.id.rv_feed)
     RecyclerView rvFeed;
 
-    @Inject
-    FeedAdapter adapter;
+    @BindView(R.id.srl)
+    SwipeRefreshLayout swipeRefreshLayout;
 
-    public static FeedFragment newInstance() {
-        return new FeedFragment();
-    }
+    @Inject
+    ViewModelProvider.Factory viewModelFactory;
+
+    private FeedViewModel viewModel;
+    private FeedAdapter adapter;
+    private OptionsDialog optionsDialog;
+    private static final String TAG = "FEED_FRAGMENT";
 
     @Override
     public void onAttach(Context context) {
         super.onAttach(context);
-
-        Timber.i("ON ATTACH");
+        viewModel = ViewModelProviders.of(getActivity(), viewModelFactory).get(FeedViewModel.class);
+        viewModel.refreshFeed();
     }
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        Timber.i("ON CREATE");
     }
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-
-        Timber.i("ON CREATE VIEW");
-
         View view = inflater.inflate(R.layout.fragment_feed, container, false);
         ButterKnife.bind(this, view);
+        swipeRefreshLayout.setOnRefreshListener(this);
+        initializeAdapter();
         return view;
     }
 
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
-
-        Timber.i("ON ACTIVITY CREATED");
+        viewModel.getUserFeed().observe(this, feed -> {
+            switch (feed.status) {
+                case ERROR:
+                    processErrorState(feed.data);
+                    break;
+                case LOADING:
+                    processLoadingState(feed.data);
+                    break;
+                case SUCCESS:
+                    if (feed.data != null)
+                        processSuccessState(feed.data);
+                    break;
+            }
+        });
     }
 
     @Override
-    public void onItemClicked(@NonNull Photo photo) {
+    public void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+    }
+
+    private void initializeAdapter() {
+        adapter = new FeedAdapter(new PhotoOnClickListener() {
+            @Override
+            public void onClick(@NonNull Photo photo) {
+                openPhotoDetails(photo);
+            }
+
+            @Override
+            public void onLongClick(@NonNull Photo photo) {
+                showOptionsDialog(photo);
+            }
+        });
+        RecyclerView.LayoutManager layoutManager = new GridLayoutManager(getContext(), 2);
+        rvFeed.setLayoutManager(layoutManager);
+        rvFeed.setAdapter(adapter);
+    }
+
+    private void processErrorState(@Nullable final List<Photo> photos) {
+        onHideLoadingView();
+        onHideSwipeLoadingView();
+        showToast(R.string.error_fetching_data);
+        adapter.updateDataSet(photos);
+    }
+
+    private void processSuccessState(@NonNull final List<Photo> feed) {
+        onHideLoadingView();
+        onHideSwipeLoadingView();
+        Timber.i("SUCCESS");
+        Toast.makeText(getContext(), "SUCC " + feed.size(), Toast.LENGTH_SHORT).show();
+        adapter.updateDataSet(feed);
+    }
+
+    private void processLoadingState(@Nullable final List<Photo> feed) {
+        onShowLoadingView();
+        adapter.updateDataSet(feed);
+    }
+
+    private void openPhotoDetails(@NonNull final Photo photo) {
 
     }
 
-    @Override
-    public void onStart() {
-        super.onStart();
+    private void showOptionsDialog(@NonNull final Photo photo) {
+        optionsDialog = OptionsDialog.newInstance(photo.getFileDownloadLink());
+        optionsDialog.show(getFragmentManager(), TAG);
+    }
 
-        Timber.i("ON START");
+    private void onShowLoadingView() {
+
+    }
+
+    private void onHideLoadingView() {
+
+    }
+
+    private void onHideSwipeLoadingView() {
+        if (swipeRefreshLayout.isRefreshing())
+            swipeRefreshLayout.setRefreshing(false);
+    }
+
+    private void showToast(final int msgId) {
+        Toast.makeText(getContext(),
+                msgId,
+                Toast.LENGTH_SHORT).show();
     }
 
     @Override
-    public void onResume() {
-        super.onResume();
-
-        Timber.i("ON RESUME");
-    }
-
-    @Override
-    public void onPause() {
-        super.onPause();
-
-        Timber.i("ON PAUSE");
-    }
-
-    @Override
-    public void onStop() {
-        super.onStop();
-
-        Timber.i("ON STOP");
-    }
-
-    @Override
-    public void onDestroyView() {
-        super.onDestroyView();
-
-        Timber.i("ON DESTROY VIEW");
-    }
-
-    @Override
-    public void onDestroy() {
-        super.onDestroy();
-
-        Timber.i("ON DESTROY");
-    }
-
-    @Override
-    public void onDetach() {
-        super.onDetach();
-
-        Timber.i("ON DETACH");
+    public void onRefresh() {
+        viewModel.refreshFeed();
     }
 }
