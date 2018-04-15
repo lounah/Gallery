c15c5c67b631165c0fb0d0db5d911b6308a0e646
Lounah <30981413+Lounah@users.noreply.github.com>
2018-04-15 18:55:41 +0300

    entities update, code clean-up

--- app/src/main/java/com/lounah/gallery/data/entity/Photo.java
+++ app/src/main/java/com/lounah/gallery/data/entity/Photo.java
@@ -3,17 +3,22 @@ package com.lounah.gallery.data.entity;
 import android.arch.persistence.room.Entity;
 import android.arch.persistence.room.Ignore;
 import android.arch.persistence.room.PrimaryKey;
+import android.os.Parcel;
+import android.os.Parcelable;
 import android.support.annotation.NonNull;
 
 import com.google.gson.annotations.SerializedName;
 
 @Entity(tableName = "photos")
-public class Photo {
+public class Photo implements Parcelable {
 
     @NonNull
     @PrimaryKey
     @SerializedName("sha256")
     private String id = "";
+
+    @SerializedName("path")
+    private String path;
 
     @SerializedName("size")
     private long size;
@@ -33,12 +38,39 @@ public class Photo {
     @Ignore
     public Photo() {}
 
-    public Photo(long size, String previewDownloadLink, String name, String fileDownloadLink, String date) {
+    public Photo(long size, String path, String previewDownloadLink, String name, String fileDownloadLink, String date) {
         this.size = size;
         this.previewDownloadLink = previewDownloadLink;
         this.name = name;
         this.fileDownloadLink = fileDownloadLink;
         this.date = date;
+        this.path = path;
+    }
+
+    protected Photo(Parcel in) {
+        id = in.readString();
+        path = in.readString();
+        size = in.readLong();
+        previewDownloadLink = in.readString();
+        name = in.readString();
+        fileDownloadLink = in.readString();
+        date = in.readString();
+    }
+
+    public static final Creator<Photo> CREATOR = new Creator<Photo>() {
+        @Override
+        public Photo createFromParcel(Parcel in) {
+            return new Photo(in);
+        }
+
+        @Override
+        public Photo[] newArray(int size) {
+            return new Photo[size];
+        }
+    };
+
+    public String getPath() {
+        return path;
     }
 
     public long getSize() {
@@ -61,11 +93,12 @@ public class Photo {
         return date;
     }
 
+    @NonNull
     public String getId() {
         return id;
     }
 
-    public void setId(String id) {
+    public void setId(@NonNull String id) {
         this.id = id;
     }
 
@@ -73,19 +106,23 @@ public class Photo {
         this.size = size;
     }
 
-    public void setPreviewDownloadLink(String previewDownloadLink) {
-        this.previewDownloadLink = previewDownloadLink;
-    }
-
     public void setName(String name) {
         this.name = name;
     }
 
-    public void setFileDownloadLink(String fileDownloadLink) {
-        this.fileDownloadLink = fileDownloadLink;
+    @Override
+    public int describeContents() {
+        return 0;
     }
 
-    public void setDate(String date) {
-        this.date = date;
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(id);
+        dest.writeString(path);
+        dest.writeLong(size);
+        dest.writeString(previewDownloadLink);
+        dest.writeString(name);
+        dest.writeString(fileDownloadLink);
+        dest.writeString(date);
     }
 }
