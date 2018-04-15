31938abac3e377c4812ef38d0ac290f69f0fc9a2
Lounah <30981413+Lounah@users.noreply.github.com>
2018-04-15 19:07:11 +0300

    code clean-up

--- app/src/main/java/com/lounah/gallery/util/AbsentLiveData.java
+++ app/src/main/java/com/lounah/gallery/util/AbsentLiveData.java
@@ -4,11 +4,12 @@ package com.lounah.gallery.util;
 import android.arch.lifecycle.LiveData;
 
 public class AbsentLiveData extends LiveData {
+
     private AbsentLiveData() {
         postValue(null);
     }
+
     public static <T> LiveData<T> create() {
-        //noinspection unchecked
         return new AbsentLiveData();
     }
 }
